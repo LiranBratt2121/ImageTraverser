2c1788b90c67df92702a10fde14433a50cec59ec
LiranBratt2121 <brattlirannin@gmail.com>
2024-06-16 17:15:27 +0300

    Make script ignore videos while also encapsulating logic

--- gui/display.py
+++ gui/display.py
@@ -1,5 +1,8 @@
 import cv2
-from os.path import join
+import numpy as np
+
+from typing import Tuple
+from os.path import join, splitext
 from .images import Images
 
 KEY_LEFT_ARROW = ord('D'), ord('d')
@@ -15,14 +18,27 @@ class Display:
         if event == cv2.EVENT_LBUTTONDOWN:
             self.images.copy_path_to_clipboard()
 
+    def __get_img(self, path: str, idx: int) -> Tuple[str, np.ndarray]:
+        supported_formats = {'.png', '.jpg', '.jpeg'}
+        _, ext = splitext(path.lower())
+        
+        if ext not in supported_formats:
+            window_name = f"Unsupported format - {idx}"
+            image = np.zeros((480, 640, 3), dtype=np.uint8)
+            cv2.putText(image, "Unsupported format", (60, 240), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 250), 2)
+        else:
+            full_image = cv2.imread(join('images', path))
+            image = cv2.resize(full_image, (480, 640))
+            window_name = f"Image - {idx}"
+
+        return window_name, image
+
     def show(self) -> None:
         key = None
         while key != KEY_QUIT:
             path, idx = self.images.get_current()
 
-            full_image = cv2.imread(join('images', path))
-            image = cv2.resize(full_image, (480, 640))
-            window_name = f"Image - {idx}"
+            window_name, image = self.__get_img(path, idx)
 
             cv2.imshow(window_name, image)
             cv2.setMouseCallback(window_name, self.copy_path_on_click)
